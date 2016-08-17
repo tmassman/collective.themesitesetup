08172d4eb284d96d74e48019a5d7f1196bf84900
Asko Soukka <asko.soukka@iki.fi>
2016-08-17 13:28:36 +0300

    Back to development: 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.themesitesetup',
-    version='1.2.0',
+    version='1.2.1.dev0',
     description='GenericSetup profiles for plone.app.theming',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
